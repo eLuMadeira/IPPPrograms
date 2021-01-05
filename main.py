40ef03094d72009b70a265e3785fd62aa58d4ae3
eLuMadeira <76945040+eLuMadeira@users.noreply.github.com>
2021-01-05 23:03:43 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+
+    user_name = input("Nice to meet ya friend. May I have your name?")
+    user_pronouns = input("So what is your pronouns? Just don't want to assume.")
+    user_hometown = input("Where are you from? Mars?")
+    user_fave_song = input("What is your favorite song then?")
+    user_pet = input("Own any pets? Cats are good answers. Dogs, not so much. :)")
+
+    print (user_name)
+    print (user_pronouns)
+    print(user_hometown)
+    print(user_fave_song)
+    print(user_pet)
